a950161f65ef7444615e77a10a534f9e8991a310
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2023-12-01 22:24:20 +0200

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Observer } from "./homework/1";
+import { RequestComponent } from "./homework/2";
+import { FormComponent } from "./homework/3";
+import { ComponentApp } from "./homework/4";
 
 function App() {
+  const testFunction = () => {
+    console.log("test");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Observer children={<div>test</div>} onContentEndVisible={testFunction} />
+      <RequestComponent />
+      <FormComponent />
+      <ComponentApp />
+    </>
   );
 }
 
